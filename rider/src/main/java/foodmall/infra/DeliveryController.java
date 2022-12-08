01c69b5a7dc118cefbd42a5954f50a775a014eab
yjinjae01 <118959734+yjinjae01@users.noreply.github.com>
2022-12-08 10:10:05 +0900

    Ver6

--- rider/src/main/java/foodmall/infra/DeliveryController.java
+++ rider/src/main/java/foodmall/infra/DeliveryController.java
@@ -20,7 +20,36 @@ public class DeliveryController {
     DeliveryRepository deliveryRepository;
 
 
+    @RequestMapping(value = "deliveries/{id}/pick",
+        method = RequestMethod.PUT,
+        produces = "application/json;charset=UTF-8")
+    public Delivery pick(@PathVariable(value = "id") Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+            System.out.println("##### /delivery/pick  called #####");
+            Optional<Delivery> optionalDelivery = deliveryRepository.findByOrderId(String.valueOf(id));
+            
+            optionalDelivery.orElseThrow(()-> new Exception("No Entity Found"));
+            Delivery delivery = optionalDelivery.get();
+            delivery.pick();
+            
+            deliveryRepository.save(delivery);
+            return delivery;
+            
+    }
 
-
+    @RequestMapping(value = "deliveries/{id}/deliveryconfirm",
+        method = RequestMethod.PUT,
+        produces = "application/json;charset=UTF-8")
+    public Delivery deliveryConfirm(@PathVariable(value = "id") Long id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+            System.out.println("##### /delivery/deliveryConfirm  called #####");
+            Optional<Delivery> optionalDelivery = deliveryRepository.findByOrderId(String.valueOf(id));
+            
+            optionalDelivery.orElseThrow(()-> new Exception("No Entity Found"));
+            Delivery delivery = optionalDelivery.get();
+            delivery.deliveryConfirm();
+            
+            deliveryRepository.save(delivery);
+            return delivery;
+            
+    }
 
 }
