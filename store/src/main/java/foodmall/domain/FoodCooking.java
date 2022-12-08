d797065406a8ac550b273d4b5ceca025a1430f2f
yjinjae01 <118959734+yjinjae01@users.noreply.github.com>
2022-12-08 10:12:28 +0900

    Ver6

--- store/src/main/java/foodmall/domain/FoodCooking.java
+++ store/src/main/java/foodmall/domain/FoodCooking.java
@@ -14,53 +14,23 @@ import java.util.Date;
 @Data
 
 public class FoodCooking  {
-
-
-    
+ 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     
-    
-    
-    
-    
     private Long id;
-    
-    
-    
-    
     
     private String orderId;
     
-    
-    
-    
-    
     private String foodId;
     
-    
-    
-    
-    
     private String status;
-    
-    
-    
-    
     
     private String address;
 
     @PostPersist
     public void onPostPersist(){
 
-
-        OrderStarted orderStarted = new OrderStarted(this);
-        orderStarted.publishAfterCommit();
-
-
-
-        OrderFinished orderFinished = new OrderFinished(this);
-        orderFinished.publishAfterCommit();
 
     }
 
@@ -72,75 +42,71 @@ public class FoodCooking  {
 
 
     public void accept(AcceptCommand acceptCommand){
-        OrderAccepted orderAccepted = new OrderAccepted(this);
-        orderAccepted.publishAfterCommit();
 
-        OrderRejected orderRejected = new OrderRejected(this);
-        orderRejected.publishAfterCommit();
+        if (acceptCommand.getAccept()){
+            
+            setStatus("OrderAccept");
+            OrderAccepted orderAccepted = new OrderAccepted(this);
+            orderAccepted.publishAfterCommit();
+        } else {
+            setStatus("OrderReject");
+            OrderRejected orderRejected = new OrderRejected(this);
+            orderRejected.publishAfterCommit();
+        }
+    }
+
+    public void start(){
+        // 요리 시작
+        setStatus("OrderStart");
+        OrderStarted orderStarted = new OrderStarted(this);
+        orderStarted.publishAfterCommit();
+
+    }
+    
+    public void finish(){
+        // 요리 종료
+        setStatus("OrderFinish");
+        OrderFinished orderFinished = new OrderFinished(this);
+        orderFinished.publishAfterCommit();
 
     }
 
     public static void orderInfo(OrderPlaced orderPlaced){
 
-        /** Example 1:  new item 
         FoodCooking foodCooking = new FoodCooking();
+        foodCooking.setFoodId(orderPlaced.getFoodId());
+        foodCooking.setOrderId(String.valueOf(orderPlaced.getId()));
+        foodCooking.setAddress(orderPlaced.getAddress());
+        foodCooking.setStatus(orderPlaced.getStatus());
+        
         repository().save(foodCooking);
 
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(orderPlaced.get???()).ifPresent(foodCooking->{
-            
-            foodCooking // do something
-            repository().save(foodCooking);
-
-
-         });
-        */
-
-        
+       
     }
+
+    // 결제 완료
     public static void updateStatus(Paid paid){
 
-        /** Example 1:  new item 
-        FoodCooking foodCooking = new FoodCooking();
-        repository().save(foodCooking);
-
-        */
-
-        /** Example 2:  finding and process
         
-        repository().findById(paid.get???()).ifPresent(foodCooking->{
+        repository().findByOrderId(paid.getOrderId()).ifPresent(foodCooking->{
             
-            foodCooking // do something
+            foodCooking.setStatus("Paid");
             repository().save(foodCooking);
 
-
          });
-        */
-
         
     }
+
+    // 주문 취소
     public static void updateStatus(OrderCanceled orderCanceled){
 
-        /** Example 1:  new item 
-        FoodCooking foodCooking = new FoodCooking();
-        repository().save(foodCooking);
 
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(orderCanceled.get???()).ifPresent(foodCooking->{
+        repository().findByOrderId(String.valueOf(orderCanceled.getId())).ifPresent(foodCooking->{
             
-            foodCooking // do something
-            repository().save(foodCooking);
+            repository().delete(foodCooking);
 
 
          });
-        */
-
         
     }
 
