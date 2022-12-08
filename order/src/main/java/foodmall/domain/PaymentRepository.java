461d6720a5b482bf664cba06cbbad80a46de09ee
yjinjae01 <118959734+yjinjae01@users.noreply.github.com>
2022-12-08 09:56:49 +0900

    Ver5

--- order/src/main/java/foodmall/domain/PaymentRepository.java
+++ order/src/main/java/foodmall/domain/PaymentRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel="payments", path="payments")
 public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long>{
 
+    Optional<Payment> findByOrderId(String id);
+
 }
