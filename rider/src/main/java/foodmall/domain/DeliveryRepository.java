237565d2c9813a94d2fae1ae651460ac029d73b2
yjinjae01 <118959734+yjinjae01@users.noreply.github.com>
2022-12-08 10:09:19 +0900

    Ver6

--- rider/src/main/java/foodmall/domain/DeliveryRepository.java
+++ rider/src/main/java/foodmall/domain/DeliveryRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel="deliveries", path="deliveries")
 public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
 
+   Optional<Delivery> findByOrderId(String orderId);
+
 }
