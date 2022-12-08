7f5c6b7b9b9c7175ff39c29c171b3586c67dea05
yjinjae01 <118959734+yjinjae01@users.noreply.github.com>
2022-12-08 10:12:52 +0900

    Ver6

--- store/src/main/java/foodmall/domain/FoodCookingRepository.java
+++ store/src/main/java/foodmall/domain/FoodCookingRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel="foodCookings", path="foodCookings")
 public interface FoodCookingRepository extends PagingAndSortingRepository<FoodCooking, Long>{
 
+  Optional<FoodCooking> findByOrderId(String orderId);
+  
 }
